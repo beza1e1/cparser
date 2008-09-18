97b3a5a1f45bb53ad3bb46de05ab59b8b1cb0dba
Christoph Mallon <christoph.mallon@gmx.de>
2008-09-18 12:16:23 +0000

    Remove trailing whitespace.
    [r22078]

--- lextest/do_tests.sh
+++ lextest/do_tests.sh
@@ -4,7 +4,6 @@ for i in tokenstreams/*; do
 	if [ "$i" != "tokenstreams/refresults" ]; then
 		echo "==> Checking $i"
 		../cparser --lextest $i > /tmp/tokenstream
-		diff -u tokenstreams/refresults/`basename $i` /tmp/tokenstream 
+		diff -u tokenstreams/refresults/`basename $i` /tmp/tokenstream
 	fi
 done
-
